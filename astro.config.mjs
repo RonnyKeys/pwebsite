0200c456f534d1712f6d10d0b461d5faeef2246b
RonnyKeys <ronnyandkeys@gmail.com>
2024-09-28 12:19:08 -0500

    lesson-65

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    prefetch: {
+        prefetchAll: true
+    },
+    site: 'https://stargazers.club',
+    integrations: [sitemap()],
+    output: 'server',
+    adapter: netlify(),
+});
+
